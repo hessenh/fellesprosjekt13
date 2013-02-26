016f497af2e89413025032827b6680f13d74604d
John-Olav Storvold <johnolav.storvold@gmail.com>
2013-02-26 20:22:20 +0100

    commit 2

---
+++ Fellesprosjekt13/src/test/TestClassRenewed.java
@@ -0,0 +1,15 @@
+package src.test;
+
+public class TestClassRenewed {
+	
+	private name;
+	
+	public TestClassRenewed(String name) {
+		this.name = name;
+	}
+
+	static public void main(String [] args) {
+		System.out.println(this.name);
+	}
+
+}
